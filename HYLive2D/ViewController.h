818e9ddd4484855cc9c0863e78b0872256ffd4e4
huyong <erpapa@huyongdeair.lan>
2017-12-09 23:23:19 +0800

    Initial Commit

---
+++ HYLive2D/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYLive2D
+//
+//  Created by huyong on 2017/12/9.
+//  Copyright © 2017年 erpapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYLive2DTests/HYLive2DTests.m
@@ -0,0 +1,39 @@
+//
+//  HYLive2DTests.m
+//  HYLive2DTests
+//
+//  Created by huyong on 2017/12/9.
+//  Copyright © 2017年 erpapa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HYLive2DTests : XCTestCase
+
+@end
+
+@implementation HYLive2DTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
